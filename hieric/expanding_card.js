36f7c90aef1f511e1531535bb1c46a78b489db45
zibosong <zibosong@gmail.com>
2025-01-15 22:15:39 -0500

    expanding cards checkin

--- hieric/expanding_card.js
+++ hieric/expanding_card.js
@@ -1,13 +1,14 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel');
 
 panels.forEach((panel) => {
 	panel.addEventListener('click', () => {
-		panel.classList.add('active')
+		removeActiveClasses();
+		panel.classList.add('active');
 	}) 
 })
 
 function removeActiveClasses() {
-	panels.forEach(panel => {
-		panel.classList.remove('active')
+	panels.forEach((panel) => {
+		panel.classList.remove('active');
 	})
 }
