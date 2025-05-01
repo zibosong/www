803d840db704660092590d9ce1e8145b103ee784
zibosong <zibosong@gmail.com>
2025-04-30 23:27:16 -0400

    update

---
+++ hieric/projects/lab/lab.js
@@ -0,0 +1,9 @@
+const pHello = document.getElementById("p_hello");
+
+//pHello.style.color = "red";
+//pHello.innerText = "yes";
+
+function changeText(newText) {
+    //pHello.style.color = newText;
+    pHello.innerText = newText;
+  }
